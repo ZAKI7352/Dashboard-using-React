5cd52c9b7a64efb8c75a15c2440600a389174943
Zaki7352 <mdzaki7352@gmail.com>
2024-10-17 18:49:54 +0530

    nav

--- project-6/src/components/Sidenav.jsx
+++ project-6/src/components/Sidenav.jsx
@@ -2,36 +2,52 @@ import React from 'react'
 import { Box, HStack, Heading, Icon, Stack, Text } from '@chakra-ui/react'
 import { RxDashboard } from "react-icons/rx";
 import { BsArrowDownUp } from "react-icons/bs";
-
-
+import { BiSupport } from "react-icons/bi";
 
 const SideNav = () => {
     const navLinks = [
         {
-          icon: RxDashboard,
-          text: "Dashboard",
-          link: "/",
+            icon: RxDashboard,
+            text: "Dashboard",
+            link: "/",
         },
         {
-          icon: BsArrowDownUp,
-          text: "Transactions",
-          link: "/transactions",
+            icon: BsArrowDownUp,
+            text: "Transactions",
+            link: "/transactions",
         },
-      ];
+    ];
+
     return (
-        <Stack boxShadow="sm" maxW="16rem"bg="red" h="100vh" >
-            <Heading textAlign="center" fontSize= "20px" as="h1" pt="3.5rem">Zaki</Heading>
-            <Box mx="3">
-            {navLinks.map((nav) => {
-                <HStack key={nav.text}>
-                    <Icon as={nav.icon} />
-                    <Text>{nav.text}</Text>
+        <Stack boxShadow="lg" maxW="16rem" h="100vh" justify="space-between">
+            <Box>
+                <Heading textAlign="center" fontSize="20px" as="h1" pt="3.5rem">Zaki</Heading>
+                <Box mx="3">
+                    {navLinks.map((nav) => (
+                        <HStack key={nav.text} py="3" px="4" borderRadius="10px" _hover={{ bg: "#F3F3F7", color: "#171717" }} color="#797E82">
+                            <Icon as={nav.icon} />
+                            <Text fontSize="14px" fontWeight="medium">{nav.text}</Text>
+                        </HStack>
+                    ))}
+                </Box>
+
+            </Box>
+
+
+            <Box mx="3" mb="6">
+                <HStack
+                    py="3"
+                    px="4"
+                    borderRadius="10px"
+                    _hover={{ bg: "#F3F3F7", color: "#171717" }}
+                    color="#797E82">
+
+                    <Icon as={BiSupport} />
+                    <Text fontSize="14px" fontWeight="medium">Support</Text>
                 </HStack>
-            })}
             </Box>
         </Stack>
-    )
+    );
 }
 
-export default SideNav
-
+export default SideNav;
